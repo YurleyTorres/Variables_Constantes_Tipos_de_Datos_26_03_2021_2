712dda53477b188ad90ac29040bc8750608c32a6
YurleyTorres <marly.torres.silva@gmail.com>
2021-04-21 21:07:28 -0500

    2021-03-26

---
+++ main.js
@@ -0,0 +1,23 @@
+//console.log("hola mundo")
+let nombre, apellido;
+
+nombre = prompt("Ingrese su nombre");
+apellido = prompt("Ingrese su apellido");
+
+//console.log(typeof nombre, typeof apellido);
+
+
+//let nombre = `Marly yurley Torres Silva`; //String
+let edad = 33; //number - intenger - int;
+let altura = 1.61; //Double - float
+let profesor = false; // Boleano
+let informacion = [nombre, edad, altura, estudiante]; //Array
+// console.log(informacion);
+// console.log(informacion[2]);
+let Vaso = {//Objeto
+	material : "vidrio",
+	diametro :  50,
+	lleno : true
+};
+console.log(Vaso);
+console.log(Vaso.diametro);
